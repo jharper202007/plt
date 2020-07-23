57671d68e6877a658a64ce890898b0b44b86d0a3
james-harper <jameswilliamharper@gmail.com>
2020-07-23 14:16:34 +0100

    Display total price

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ProductsList from './components/Products/ProductsList';
+import TotalPrice from './components/Products/TotalPrice/index';
 
 function App() {
   return (
     <div className="container">
       <ProductsList />
+      <div className="row">
+        <div className="col-2 offset-10">
+          <div className="col-4 offset-4"><TotalPrice/></div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Products/TotalPrice/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { AppState } from './../../../reducers';
+
+const TotalPrice = () => {
+  const total = useSelector((state: AppState) => state.basket.total);
+
+  return (
+    <span><strong>Total: </strong>£{total.toFixed(2)}</span>
+  );
+};
+
+export default TotalPrice;
