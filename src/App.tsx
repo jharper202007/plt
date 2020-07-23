e4e7da2d8a751f4462a552f387cc8f3e1ceea7c5
james-harper <jameswilliamharper@gmail.com>
2020-07-23 12:38:46 +0100

    Basic product list styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ProductsList from './components/Products/ProductsList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <ProductsList />
     </div>
   );
--- src/components/Products/ProductsListItem/index.tsx
+++ src/components/Products/ProductsListItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Product } from '../../../types';
+import './ProductsListItem.css';
 
 interface ProductsListItemProps {
   product: Product;
@@ -8,8 +9,25 @@ interface ProductsListItemProps {
 
 const ProductsListItem = ({ product }: ProductsListItemProps) => {
   return (
-    <div>
-      {product.name}
+    <div className="card mb-1">
+      <div className="row">
+        <div className="col-2">
+          <img src={product.img} height={150} width={150} />
+        </div>
+        <div className="col-8">
+          <div className="card-block p-2">
+            <h5 className="card-title">{product.name}</h5>
+            <p className="card-text">£{product.price.toFixed(2)}</p>
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="row h-100 align-items-center">
+            <div className="col">-</div>
+            <div className="col">Qty</div>
+            <div className="col">+</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
